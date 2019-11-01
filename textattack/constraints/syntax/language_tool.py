17d18bdf5c635ac2ea954d129265a1fc7122634e
Jack Morris <jxmorris12@gmail.com>
2019-11-01 13:42:07 -0400

    use original text for languagetool comparison and only cache examples that will be repeated; delete old file

--- textattack/constraints/syntax/language_tool.py
+++ textattack/constraints/syntax/language_tool.py
@@ -18,18 +18,20 @@ class LanguageTool(Constraint):
         self.threshold = threshold
         self.grammar_error_cache = {}
     
-    def get_errors(self, tokenized_text):
+    def get_errors(self, tokenized_text, use_cache=False):
         text = tokenized_text.text
-        if text not in self.grammar_error_cache:
-            num_errors = len(self.lang_tool.check(text))
-            self.grammar_error_cache[text] = num_errors
-        return self.grammar_error_cache[text]
+        if use_cache:
+            if text not in self.grammar_error_cache:
+                self.grammar_error_cache[text] = len(self.lang_tool.check(text))
+            return self.grammar_error_cache[text]
+        else:
+            return len(self.lang_tool.check(text))
     
     def call_many(self, x, x_adv_list, original_text=None):
-        return [x_adv for x_adv in x_adv_list if self.__call__(x, x_adv)]
+        return [x_adv for x_adv in x_adv_list 
+                if self.__call__(x, x_adv, original_text=original_text)]
     
-    def __call__(self, x, x_adv):
-        errors_added = self.get_errors(x_adv) - self.get_errors(x)
-        print(self.get_errors(x_adv),'/',self.get_errors(x))
-        print('\t',errors_added)
+    def __call__(self, x, x_adv, original_text=None):
+        original_num_errors = self.get_errors(original_text, use_cache=True)
+        errors_added = self.get_errors(x_adv) - original_num_errors
         return errors_added <= self.threshold
