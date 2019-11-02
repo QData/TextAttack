e2df36ba71a743c4fe7188fd9c1f9b3f21a06014
k-ivey <kivey1@gmail.com>
2019-11-02 17:45:53 -0400

    Add attacks and constraints

--- textattack/run.py
+++ textattack/run.py
@@ -8,12 +8,12 @@ import argparse
 parser = argparse.ArgumentParser(description='A commandline parser for TextAttack')
 
 parser.add_argument('--attack', type=str, required=True, 
-    choices=['greedy-counterfit', 'ga-counterfit'], help='The type of attack to run')
+    choices=['greedy-counterfit', 'ga-counterfit', 'greedy-wir'], help='The type of attack to run')
 
 parser.add_argument('--model', type=str, required=True, 
-    choices=['bert-sentiment'], help='The classification model to attack')
+    choices=['bert-sentiment', 'infer-sent'], help='The classification model to attack')
 
-parser.add_argument('--constraint', type=float, required=False, 
+parser.add_argument('--constraints', type=str, required=False, nargs='*',
     help='A constraint to add to the attack')
 
 parser.add_argument('--data', type=str, required=True, 
@@ -31,25 +31,71 @@ if __name__ == '__main__':
     import constraints
     import transformations
     import datasets
+    import time
 
+    start_time = time.time()
+
+    #Models
     if args.model == 'bert-sentiment':
         model = models.BertForSentimentClassification()
+    elif args.model == 'infer-sent':
+        #Doesn't work for now
+        raise NotImplementedError()
+        model = models.InferSent()
     
-    transformation = transformations.WordSwapCounterfit()
+    #Transformation
+    transformation = transformations.WordSwapEmbedding()
 
+    #Attacks
     if args.attack == 'greedy-counterfit':
         attack = attacks.GreedyWordSwap(model, transformation)
     elif args.attack == 'ga-counterfit':
         attack = attacks.GeneticAlgorithm(model, transformation)
+    elif args.attack == 'greedy-wir':
+        attack = attacks.GreedyWordSwapWIR(model, transformation)
 
-    if args.constraint:
-        attack.add_constraints((
-            constraints.semantics.UniversalSentenceEncoder(args.constraint, metric='cosine'),
-        ))
+    #Constraints
+    if args.constraints:
 
+        defined_constraints = []
+
+        for constraint in args.constraints:
+            if 'sim:' in constraint:
+                similarity = constraint.replace('sim:', '')
+
+                defined_constraints.append(constraints.semantics.UniversalSentenceEncoder(float(similarity), metric='cosine'),)
+
+                # attack.add_constraints(
+                #     (
+                #     constraints.semantics.UniversalSentenceEncoder(float(similarity), metric='cosine'),
+                #     )
+                # )
+            elif 'lang-tool' in constraint:
+                threshold = constraint.replace('lang-tool:', '')
+
+                defined_constraints.append(constraints.syntax.LanguageTool(float(threshold)),)
+
+                # attack.add_constraints(
+                #     (
+                #     constraints.syntax.LanguageTool(float(threshold)),
+                #     )
+                # )
+
+        attack.add_constraints(defined_constraints)
+
+    #Data
     if args.data == 'yelp-sentiment':
         data = datasets.YelpSentiment(args.n)
 
+
+    load_time = time.time()
+
     attack.attack(data, shuffle=False)
+
+    finish_time = time.time()
+
+    print(f'Loaded in {load_time - start_time}s')
+    print(f'Ran attack in {finish_time - load_time}s')
+    print(f'TOTAL TIME: {finish_time - start_time}s')
         
 
