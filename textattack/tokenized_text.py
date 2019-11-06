c5ce7ef10e65974d511c37cbb899d0b874c59238
uvafan <eli.d.lifland@gmail.com>
2019-11-06 16:36:20 -0500

    USE fixes

--- textattack/tokenized_text.py
+++ textattack/tokenized_text.py
@@ -1,13 +1,16 @@
 from utils import get_device
 
 class TokenizedText:
-    def __init__(self, model, text):
+    def __init__(self, model, text, modified_index=None):
         """ Initializer stores text and tensor of tokenized text.
         """
         self.model = model
         self.text = text
         self.ids = model.convert_text_to_ids(text)
         self.raw_words = raw_words(text)
+        self.attack_attrs = dict()
+        if modified_index:
+            self.attack_attrs['modified_index'] = modified_index
     
     def words(self):
         """ Returns the distinct words from self.text. 
@@ -15,28 +18,41 @@ class TokenizedText:
             @TODO Should we ever consider case when substituting words?
         """
         return self.raw_words
-    
-    def text_until_word_index(self, i):
-        """ Returns the text before the beginning of word at index `i`. """
-        pre_words = self.words()[:i+1]
+
+    def text_window_around_index(self, index, size):
+        length = len(self.raw_words)
+        half_size = (size - 1) // 2
+        if index - half_size < 0:
+            start = 0
+            end = min(size, length-1)
+        elif index + half_size > length - 1:
+            start = max(0, length - size)
+            end = length - 1
+        else:
+            start = index - half_size
+            end = index + half_size
+        text_idx_start = self._text_index_of_word_index(start)
+        text_idx_end = self._text_index_of_word_index(end) + len(self.raw_words[end])
+        return self.text[text_idx_start:text_idx_end]
+         
+    def _text_index_of_word_index(self, i):
+        pre_words = self.raw_words[:i+1]
         lower_text = self.text.lower()
         # Find all words until `i` in string.
         look_after_index = 0
         for word in pre_words:
             look_after_index = lower_text.find(word, look_after_index)
+        return look_after_index 
+
+    def text_until_word_index(self, i):
+        """ Returns the text before the beginning of word at index `i`. """
+        look_after_index = self._text_index_of_word_index(i)
         return self.text[:look_after_index]
     
     def text_after_word_index(self, i):
-        """ Returns the text before the beginning of word at index `i`. """
-        word = self.words()[i]
-        pre_words = self.words()[:i+1]
-        lower_text = self.text.lower()
-        # Find all words until `i` in string.
-        look_after_index = 0
-        for word in pre_words:
-            look_after_index = lower_text.find(word, look_after_index)
-        # Jump to the end of word.
-        look_after_index += len(word)
+        """ Returns the text after the end of word at index `i`. """
+        # Get index of beginning of word then jump to end of word.
+        look_after_index = self._text_index_of_word_index(i) + len(self.raw_words[i])
         return self.text[look_after_index:]
     
     def first_word_diff(self, other_tokenized_text):
@@ -79,7 +95,7 @@ class TokenizedText:
             return True
         return w1[i] != w2[i]
 
-    def replace_words_at_indices(self, indices, words):
+    def replace_words_at_indices(self, indices, words, modified_index=None):
         """ This code returns a new TokenizedText object where the word at 
             `index` is replaced with a new word."""
         if len(indices) != len(words):
@@ -87,14 +103,14 @@ class TokenizedText:
         new_words = self.raw_words[:]
         for i, word in zip(indices, words):
             new_words[i] = word
-        return self.replace_new_words(new_words)
+        return self.replace_new_words(new_words, modified_index=modified_index)
     
     def replace_word_at_index(self, index, new_word):
         """ This code returns a new TokenizedText object where the word at 
             `index` is replaced with a new word."""
-        return self.replace_words_at_indices([index], [new_word])
+        return self.replace_words_at_indices([index], [new_word], index)
     
-    def replace_new_words(self, new_words):
+    def replace_new_words(self, new_words, modified_index=None):
         """ This code returns a new TokenizedText object and replaces old list 
             of words with a new list of words, but preserves the punctuation 
             and spacing of the original message.
@@ -109,7 +125,7 @@ class TokenizedText:
             final_sentence += adv_word
             text = text[word_end:]
         final_sentence += text # Add all of the ending punctuation.
-        return TokenizedText(self.model, final_sentence)
+        return TokenizedText(self.model, final_sentence, modified_index=modified_index)
         
     def __repr__(self):
         return self.text
