97bb61f7756877de37081218c622b80e6fb9963a
k-ivey <kivey1@gmail.com>
2019-11-07 21:53:29 -0500

    Add function parameters to parsing

--- textattack/run_attack.py
+++ textattack/run_attack.py
@@ -6,9 +6,12 @@ A command line parser to run an attack
 import argparse
 import time
 import os
+import inspect
+import re
+import ast
 
-import textattack.attacks as attacks
 import textattack.datasets as datasets
+import textattack.attacks as attacks
 import textattack.models as models
 import textattack.constraints as constraints
 import textattack.transformations as transformations
@@ -24,23 +27,26 @@ DATASET_CLASS_NAMES = {
     'yelp':             datasets.classification.YelpSentiment
 }
 
+
 def get_args():
     parser = argparse.ArgumentParser(description='A commandline parser for TextAttack', 
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     
     data_group = parser.add_mutually_exclusive_group(required=True)
     
-    parser.add_argument('--attack', type=str, required=False, default='greedy-wir-counterfit',
-        choices=['greedy-counterfit', 'ga-counterfit', 'greedy-wir-counterfit'], 
+    parser.add_argument('--attack', type=str, required=False, default='greedy-wir-counterfit', 
         help='The type of attack to run.')
     
     parser.add_argument('--model', type=str, required=False, default='bert-sentiment',
-        choices=['bert-sentiment'], help='The classification model to attack.')
+        help='The classification model to attack.')
+
+    parser.add_argument('--transformations', type=str, required=False, nargs='*',
+        default=['word-swap-embedding'], help='Transformation(s) to add to the attack')
     
     parser.add_argument('--constraints', type=str, required=False, nargs='*',
-        default=['use', 'lang-tool'], 
-        help=('Constraints to add to the attack. Usage: "--constraints use lang-tool:{threshold} ' 
-        'goog-lm" to use the default use similarity of .9 and to choose the threshold'))
+        default=['use'], 
+        help=('Constraints to add to the attack. Usage: "--constraints {constraint}:{arg_1}={value_1},{arg_3}={value_3}' 
+        ' Options are use, lang-tool, and goog-lm'))
     
     parser.add_argument('--out_file', type=str, required=False,
         help='The file to output the results to.')
@@ -59,6 +65,27 @@ def get_args():
     return args
 
 
+def get_params(func_callable, function_params):
+
+    # Break the string passed into argparse into the function and each parameter
+    param_list = re.split(':|,', function_params)
+
+    sig = inspect.signature(func_callable)
+
+    # Make a dictionary mapping each parameter to its default value or None
+    args = dict(str(item).split("=") if len(str(item).split("=")) == 2 else [str(item), None] for item in sig.parameters.values())
+
+    for param in param_list[1:]:
+        keyword, value = param.split('=')
+
+        if keyword not in args.keys():
+            raise ValueError(f'{keyword} is not a valid parameter for {param_list[0]}')
+        else:
+            args[keyword] = value
+
+    return args
+
+
 if __name__ == '__main__':
     args = get_args()
     
@@ -70,19 +97,60 @@ if __name__ == '__main__':
     start_time = time.time()
 
     # Models
-    if args.model == 'bert-sentiment':
-        model = models.BertForSentimentClassification()
+    if 'bert-sentiment' in args.model:
+        params = get_params(models.BertForSentimentClassification, args.model)
+        model = models.BertForSentimentClassification(max_seq_length=float(params['max_seq_length']))
+
+    else:
+        raise ValueError('Model must be bert-sentiment')
     
-    # Transformation
-    transformation = transformations.WordSwapEmbedding()
+    # Transformations
+    if args.transformations:
+
+        added_transformations = []
+
+        for transformation in args.transformations:
+
+            if 'word-swap-embedding' in transformation:
+                params = get_params(transformations.WordSwapEmbedding, transformation)
+                added_transformations.append(transformations.WordSwapEmbedding(
+                    replace_stopwords = ast.literal_eval(params['replace_stopwords']),
+                    word_embedding = params['word_embedding'].replace("'", ''),
+                    similarity_threshold = ast.literal_eval(params['similarity_threshold'])
+                ))
+
+            else:
+                raise ValueError((f'{transformation} is not a valid transformation type. '
+                'Valid transformations are word-swap-embedding'))
 
     # Attacks
-    if args.attack == 'greedy-counterfit':
-        attack = attacks.GreedyWordSwap(model, transformation)
-    elif args.attack == 'ga-counterfit':
-        attack = attacks.GeneticAlgorithm(model, transformation)
-    elif args.attack == 'greedy-wir-counterfit':
-        attack = attacks.GreedyWordSwapWIR(model, transformation)
+    if 'greedy-counterfit' in args.attack:
+        params = get_params(attacks.GreedyWordSwap, args.attack)
+        attack = attacks.GreedyWordSwap(
+            model, 
+            added_transformations, 
+            max_depth=float(params['max_depth']
+        ))
+
+    elif 'ga-counterfit' in args.attack:
+        params = get_params(attacks.GeneticAlgorithm, args.attack)
+        attack = attacks.GeneticAlgorithm(
+            model, 
+            added_transformations, 
+            pop_size=int(params['pop_size']),
+            max_iters=int(params['max_iters'])
+            )
+
+    elif 'greedy-wir-counterfit' in args.attack:
+        params = get_params(attacks.GreedyWordSwapWIR, args.attack)
+        attack = attacks.GreedyWordSwapWIR(
+            model, 
+            added_transformations, 
+            max_depth=float(params['max_depth']
+        ))
+
+    else:
+        raise ValueError(f'{args.attack} is not a valid attack')
 
     # Constraints
     if args.constraints:
@@ -90,22 +158,27 @@ if __name__ == '__main__':
         defined_constraints = []
 
         for constraint in args.constraints:
-            if 'use:' in constraint:
-                similarity = constraint.replace('use:', '')
-                defined_constraints.append(constraints.semantics.UniversalSentenceEncoder(float(similarity), metric='cosine'))
-            elif constraint == 'use':
-                # Default similarity to .90 if no similarity is given
-                defined_constraints.append(constraints.semantics.UniversalSentenceEncoder(.90, metric='cosine'))
 
-            elif 'lang-tool:' in constraint:
-                threshold = constraint.replace('lang-tool:', '')
-                defined_constraints.append(constraints.syntax.LanguageTool(float(threshold)))
-            elif constraint == 'lang-tool':
-                # Default threshold to 0 if no threshold is given
-                defined_constraints.append(constraints.syntax.LanguageTool(0))
+            if 'use' in constraint:
+                params = get_params(constraints.semantics.UniversalSentenceEncoder, constraint)
+                defined_constraints.append(constraints.semantics.UniversalSentenceEncoder(
+                    threshold=float(params['threshold']), 
+                    metric=params['metric'].replace("'", '')
+                    ))
 
-            elif constraint == 'goog-lm':
-                defined_constraints.append(constraints.semantics.google_language_model.GoogleLanguageModel())
+            elif 'lang-tool' in constraint:
+                params = get_params(constraints.syntax.LanguageTool, constraint)
+                defined_constraints.append(constraints.syntax.LanguageTool(
+                    threshold=float(params['threshold'])
+                    ))
+
+            elif 'goog-lm' in constraint:
+                params = get_params(constraints.semantics.google_language_model.GoogleLanguageModel, constraint)
+                defined_constraints.append(constraints.semantics.google_language_model.GoogleLanguageModel(
+                    top_n = ast.literal_eval(params['top_n']),
+                    top_n_per_index = ast.literal_eval(params['top_n_per_index']),
+                    print_step = ast.literal_eval(params['print_step'])
+                ))
 
             else:
                 raise ValueError((f'{constraint} is not a valid constraint. ' 
@@ -117,8 +190,6 @@ if __name__ == '__main__':
     if args.data in DATASET_CLASS_NAMES:
         dataset_class = DATASET_CLASS_NAMES[args.data]
         data = dataset_class(args.num_examples)
-    else:
-        raise ValueError(f'Unknown dataset {args.data}')
 
     # Output file
     if args.out_file is not None:
