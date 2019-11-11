392be1e5ebdc697ec05659a03f87ec1aa91d1be0
Jack Morris <jxmorris12@gmail.com>
2019-11-10 22:10:15 -0500

    add recipes from papers

--- textattack/__init__.py
+++ textattack/__init__.py
@@ -8,6 +8,7 @@ from . import constraints
 from . import datasets
 from . import models
 from . import outputs
+from . import recipes
 from . import run_attack
 from . import tokenized_text
 from . import transformations
---
+++ textattack/attack-recipes/alzantot_2018_genetic_algorithm.py
@@ -0,0 +1,36 @@
+'''
+    Alzantot, M., Sharma, Y., Elgohary, A., Ho, B., Srivastava, M.B., & Chang, 
+        K. (2018). 
+    
+    Generating Natural Language Adversarial Examples. 
+    
+    EMNLP. 
+    
+    ArXiv, abs/1801.00554.
+'''
+
+from textattack.attacks.blackbox import GeneticAlgorithm
+from textattack.constraints.semantics import GoogleLanguageModel
+from textattack.transformations import WordSwapEmbedding
+
+def Alzantot2018GeneticAlgorithm():
+    #
+    # Swap words with their embedding nearest-neighbors. 
+    #
+    # Embedding: Counter-fitted Paragram Embeddings.
+    #
+    # 50 nearest-neighbors with a cosine similarity of at least 0.7.
+    #
+    transformation = WordSwapEmbedding()
+    #
+    # Perform word substitution with a genetic algorithm.
+    #
+    attack = GeneticAlgorithm(model, transformation)
+    #
+    # Language Model (TODO-- what threshold?)
+    #
+    attack.add_constraint(
+            GoogleLanguageModel()
+    )
+    
+    return attack
