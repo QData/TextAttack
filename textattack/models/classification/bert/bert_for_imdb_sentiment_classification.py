cfd0236ec3e244d6be3740159e099bac33edb4de
Jack Morris <jxmorris12@gmail.com>
2019-11-12 22:50:35 -0500

    remove comments

--- textattack/models/classification/bert/bert_for_imdb_sentiment_classification.py
+++ textattack/models/classification/bert/bert_for_imdb_sentiment_classification.py
@@ -5,7 +5,6 @@ class BERTForIMDBSentimentClassification(BERTForClassification):
     BERT fine-tuned on the Yelp Sentiment dataset for sentiment classification.
     """
     
-    # MODEL_PATH = '/p/qdata/jm8wx/research/text_attacks/RobustNLP/BertClassifier/outputs/imdb-2019-11-08-17:12'
     MODEL_PATH_CASED = '/p/qdata/jm8wx/research/text_attacks/RobustNLP/BertClassifier/outputs/imdb-cased-2019-11-12-05:04'
     MODEL_PATH_UNCASED = '/p/qdata/jm8wx/research/text_attacks/RobustNLP/BertClassifier/outputs/imdb-uncased-2019-11-11-19:57'
     def __init__(self, cased=False):
--- textattack/models/classification/bert/bert_for_mr_sentiment_classification.py
+++ textattack/models/classification/bert/bert_for_mr_sentiment_classification.py
@@ -5,7 +5,6 @@ class BERTForMRSentimentClassification(BERTForClassification):
     BERT fine-tuned on the Yelp Sentiment dataset for sentiment classification.
     """
     
-    # MODEL_PATH = '/p/qdata/jm8wx/research/text_attacks/RobustNLP/BertClassifier/outputs/mr-2019-11-08-16:51/'
     MODEL_PATH_CASED = '/p/qdata/jm8wx/research/text_attacks/RobustNLP/BertClassifier/outputs/mr-cased-2019-11-11-20:52'
     MODEL_PATH_UNCASED = '/p/qdata/jm8wx/research/text_attacks/RobustNLP/BertClassifier/outputs/mr-uncased-2019-11-11-11:17'
     def __init__(self):
