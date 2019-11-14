a20e225c7f6482e9fa361a19cabaeb840ec1bd31
Jack Morris <jxmorris12@gmail.com>
2019-11-14 12:17:38 -0500

    add args to recipes

--- benchmarking/recipes.py
+++ benchmarking/recipes.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import textattack
 import sys
@@ -7,6 +8,7 @@ from textattack.attack_recipes import Alzantot2018GeneticAlgorithm, Jin2019TextF
 from textattack import models
 
 def _cb(s): return textattack.utils.color_text_terminal(str(s), color='blue')
+def _cg(s): return textattack.utils.color_text_terminal(str(s), color='green')
 def _cr(s): return textattack.utils.color_text_terminal(str(s), color='red')
 
 
@@ -20,30 +22,28 @@ def test_attack(attack, dataset):
     print(f'TOTAL TIME: {finish_time - start_time}s')
     
 def test_all_recipes(recipes, model, datasets):
-    print('datasets:',datasets)
-    
     for dataset_name, dataset in datasets:
         for recipe_class in recipes:
             recipe = recipe_class(model)
             print(f'\nTesting {_cb(recipe_class.__name__)} on {_cb(dataset_name)}...')
             test_attack(recipe, dataset)
             print()
-    print('-' * 60)
+    print(_cg('-') * 60)
 
-def test_model(recipes, args.n):
+def test_model(recipes, num_examples):
     model = models.classification.bert.BERTForIMDBSentimentClassification()
     
     datasets = (
-        ('IMDBSentiment', textattack.datasets.classification.IMDBSentiment(5)),
+        ('IMDBSentiment', textattack.datasets.classification.IMDBSentiment(num_examples)),
     )
     
-    test_all_recipes(recipes, model, datasets))
+    test_all_recipes(recipes, model, datasets)
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--recipe', '--r', default=None, type=str,
         help="recipe to test (if you dont want to test them all)")
-    parser.add_argument('--n', type=int, default=100, 
+    parser.add_argument('--num_examples', '--n', type=int, default=100, 
         help="number of examples to test on")
     return parser.parse_args()
 
@@ -63,13 +63,13 @@ if __name__ == '__main__':
     
     # Get recipe name param.
     recipe_name = args.recipe
-    if recipe_name == 'all':
+    if recipe_name is None or recipe_name == 'all':
         recipes = [Alzantot2018GeneticAlgorithm, Jin2019TextFooler]
     elif recipe_name in ['alzantot', 'alzantot2018geneticalgorithm']:
         recipes = [Alzantot2018GeneticAlgorithm]
     elif recipe_name in ['textfooler', 'jin2019textfooler']:
         recipes = [Jin2019TextFooler]
     else:
-        raise ValueErrorf('Invalid recipe_name {sys.argv[1]}')
+        raise ValueError('Invalid recipe_name {args.recipe}')
     
-    test_model(recipes, model)
+    test_model(recipes, args.num_examples)
