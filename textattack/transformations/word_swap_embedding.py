744f9446afe21e7cde83d9e69ec9800631427287
Jack Morris <jxmorris12@gmail.com>
2019-11-28 23:22:51 -0500

    recover word case after swap

--- textattack/transformations/word_swap_embedding.py
+++ textattack/transformations/word_swap_embedding.py
@@ -48,10 +48,23 @@ class WordSwapEmbedding(WordSwap):
             word_id = self.word_embedding_word2index[word.lower()]
             nnids = self.nn[word_id][1:self.max_candidates+1]
             candidate_words = []
-            # @TODO vectorize.
             for i, nbr_id in enumerate(nnids):
-                candidate_words.append(self.word_embedding_index2word[nbr_id])
+                nbr_word = self.word_embedding_index2word[nbr_id]
+                candidate_words.append(recover_word_case(nbr_word, word))
             return candidate_words
         except KeyError:
             # This word is not in our word embedding database, so return an empty list.
             return []
+
+def recover_word_case(word, reference_word):
+    """ Makes the case of `word` like the case of `reference_word`. Supports 
+        lowercase, UPPERCASE, and Capitalized. """
+    if reference_word.islower():
+        return word.lower()
+    elif reference_word.isupper():
+        return word.upper()
+    elif reference_word[0].isupper() and reference_word[1:].islower():
+        return word.capitalize()
+    else:
+        # if other, just do not alter the word's case
+        return word
