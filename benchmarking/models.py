0dce7ccb1cfd9660a2bc1d38a13916575841e17e
Jack Morris <jxmorris12@gmail.com>
2019-12-03 21:30:41 -0500

    working on models script

--- benchmarking/models.py
+++ benchmarking/models.py
@@ -10,39 +10,51 @@ def _cg(s): return textattack.utils.color(str(s), color='green', method='stdout'
 def _cr(s): return textattack.utils.color(str(s), color='red', method='stdout')
 def _pb(): print(_cg('-' * 60))
 
+from collections import Counter
 
 def get_num_successes(model, ids, true_labels):
-    ids = torch.stack(ids)
+    # import pdb; pdb.set_trace()
+    ids = map(torch.tensor, zip(*ids))
+    ids = (x.to(textattack.utils.get_device()) for x in ids)
     true_labels = torch.tensor(true_labels).to(textattack.utils.get_device())
-    preds = model(ids)
+    preds = model(*ids)
     guess_labels = preds.argmax(dim=1)
     successes = (guess_labels == true_labels).sum().item()
-    return successes
+    return successes, true_labels, guess_labels
 
-def test_model_on_dataset(model, dataset, batch_size=256):
+def test_model_on_dataset(model, dataset, batch_size=16):
     # TODO do inference in batch.
     succ = 0
     fail = 0
     batch_ids = []
     batch_labels = []
+    all_true_labels = []
+    all_guess_labels = []
     for label, text in dataset:
         ids = model.tokenizer.encode(text)
-        ids = torch.tensor(ids).to(textattack.utils.get_device())
         batch_ids.append(ids)
         batch_labels.append(label)
         if len(batch_ids) == batch_size:
-            batch_succ = get_num_successes(model, batch_ids, batch_labels)
+            batch_succ, true_labels, guess_labels = get_num_successes(model, batch_ids, batch_labels)
             batch_fail = batch_size - batch_succ
             succ += batch_succ
             fail += batch_fail
             batch_ids = []
             batch_labels = []
+            all_true_labels.extend(true_labels.tolist())
+            all_guess_labels.extend(guess_labels.tolist())
     # predict remainder batch
     if len(batch_ids) > 0:
-        batch_succ = get_num_successes(model, batch_ids, batch_labels)
+        batch_succ, true_labels, guess_labels = get_num_successes(model, batch_ids, batch_labels)
         batch_fail = len(batch_ids) - batch_succ
         succ += batch_succ
         fail += batch_fail
+        all_true_labels.extend(true_labels.tolist())
+        all_guess_labels.extend(guess_labels.tolist())
+    
+    print('true_labels:', Counter(all_true_labels))
+    print('guess_labels:', Counter(all_guess_labels))
+    
     perc = float(succ)/(succ+fail)*100.0
     perc = '{:.2f}%'.format(perc)
     print(f'Successes {succ}/{succ+fail} ({_cb(perc)})')
