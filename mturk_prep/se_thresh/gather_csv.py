fe6226ae87194870964171a433bf9852205ce885
uvafan <eli.d.lifland@gmail.com>
2019-12-05 19:35:09 -0500

    script for gathering csv for mturk

---
+++ mturk_prep/se_thresh/gather_csv.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+
+input_path = '../../textattack/outputs/'
+output_path = 'examples.csv'
+
+def main():
+    df = pd.DataFrame()
+    for run_folder in os.listdir(input_path):
+        run_path = os.path.join(input_path, run_folder)
+        run_files = os.listdir(run_path)
+        if 'final.txt' not in run_files:
+            continue
+        args_path = os.path.join(run_path,'args.txt')
+        args_lines = open(args_path, 'r').readlines()
+        se_thresh_idx = args_lines[0].find('tf-adjusted') + 12
+        se_thresh = args_lines[0][se_thresh_idx:se_thresh_idx+3]
+        words = args_lines[0].split()
+        dataset = ''
+        for word in words:
+            if word[:5] == 'bert-':
+                dataset = word[5:]
+        for f in os.listdir(run_path):
+            if f.find('.csv') == -1:
+                continue
+            add_df = pd.read_csv(os.path.join(run_path,f), index_col=0)
+            add_df['SE_Thresh'] = se_thresh
+            add_df['dataset'] = dataset
+            df = df.append(add_df)
+    df.to_csv(output_path)
+
+if __name__ == '__main__':
+    main()
