ef4f6a61d2ab31255065288e1aa2c6ba03d7e7ad
k-ivey <kivey1@gmail.com>
2020-02-02 15:48:37 -0500

    Add dataset tests

--- textattack/test/test.py
+++
@@ -1,9 +0,0 @@
-
-""" @TODO: 
-    - Test downloading models from online
-    - Load yelp model and test accuracy
-    - Test a greedy attack
-    - Test a greedy attack w/ word constraint
-    - Test a greedy attack w/ sentence constraint
-    - Test a greedy WIR attack
-"""
