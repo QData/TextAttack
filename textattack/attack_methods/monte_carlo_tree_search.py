57a638e5ce0effa1048eecc8d7909f11b9fe8f5c
Jin Yong Yoo <jy2ma@virginia.edu>
2020-02-12 12:26:42 -0500

    wip

--- textattack/attack_methods/monte_carlo_tree_search.py
+++ textattack/attack_methods/monte_carlo_tree_search.py
@@ -1,42 +1,106 @@
 from .attack import Attack
 from textattack.attack_results import AttackResult, FailedAttackResult
 import numpy as np
-import math, random
+import math, random, statistics
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
 
+NODE_ID = 0
+
 DEBUG = True
+import cProfile, pstats, io
+from pstats import SortKey
+
+class TransformationCache:
+
+    def __init__(self):
+        self.cache = {}
+
+    def get_transformation(i, new_word):
+        """
+        Args:
+            i (int) : i-th word to transform
+            word (str): transformed word
+        Returns:
+            TokenizedText
+        """
+        if i in self.cache.keys() and word in self.cache[i].keys() > 0:
+            return self.cache[i][new_word]
+        else:
+            return None
+
+    def get_random_transformation(indices_to_transform):
+        i = random.choice(indices_to_transform)
+        if i in self.cache.keys():
+            words 
+
+        if word in self.cache.keys() and transformation in self.cache[i]
+        if word in self.cache.keys() and len(self.cache[word]) > 0:
+            return random.choice(self.cache[word])
+        else:
+            return None
+
+    def store_transformation(word, transformations):
+        self.cache[word] = transformations
+
 
 class Node:
 
-    def __init__(self, depth, parent):
+    """
+        Represents a node in search tree
+        Members:
+            state (TokenizedText) : Current version of TokenizedText
+            action_seq : list of actions that led to current state
+            depth : Current depth in search tree
+            parent : Parent node
+            num_visits : Number of visits to the current node
+            ...
+    """
+
+    def __init__(self, text, depth, parent):
+        self.id = NODE_ID
+        NODE_ID += 1
+        self.state = text
         self.depth = depth
         self.parent = parent
         self.num_visits = 0
         self.value = 0.0
+        self.local_rave_values = {} # Maps action (int, str) --> value (int)
+        self.value_history = []
+        self.variance = 0.0
         self.children = {}
 
     def is_leaf(self):
         return not bool(self.children)
 
-class SearchTree:
+    def calc_variance(self):
+        self.variance = statistics.variance(self.value_history, self.value)
 
-    def __init__(self, original_text, original_label, original_score, max_words_changed):
-        self.root = Node(0, None)
+class SearchTree:
+    """
+        Object used to hold states/variables for a specific run of MCTS
+    """
+
+    def __init__(self, original_text, original_label, original_confidence, max_depth):
+        self.root = Node(original_text, 0, None)
         self.original_text = original_text
         self.original_label = original_label
-        self.orignal_score = original_score
-        self.max_depth = min(max_words_changed, len(original_text.words))
+        self.orignal_confidence = original_confidence
+        self.max_depth = max_depth 
 
-        self.unchanged_words = set(range(len(original_text.words)))
+        self.available_words_to_replace = set(range(len(original_text.words)))
+        self.actions_to_skip = set()
+
         self.game_value = 0.0
-        self.terminate = False
+        self.global_rave_values = {} # Maps action (int, str) --> value (int)
+
+        self.terminate = False  # probably won't need this
 
     def reset_tree(self):
-        self.unchanged_words = set(range(len(original_text.words)))
+        self.available_words_to_replace = set(range(len(original_text.words)))
         self.game_value = 0.0
         self.terminate = False
 
@@ -47,44 +111,36 @@ class MonteCarloTreeSearch(Attack):
         model: A PyTorch or TensorFlow model to attack.
         transformation: The type of transformation to use. Should be a subclass of WordSwap. 
         constraints: A list of constraints to add to the attack
-        reward_type (str): Defines what type of function to use for MCTS.
-            - prob_diff: Uses "max_{c'} P(c'|s) - P(c_l|s)"
             - entropy: Uses negative log-likelihood function
-        max_iter (int) : Maximum iterations for MCTS. Default is 4000
+        num_iter (int) : Number of iterations for MCTS. Default is 4000
         max_words_changed (int) : Maximum number of words we change during MCTS. Effectively represents depth of search tree.
     """
-    def __init__(self, model, transformation, constraints=[], 
-        reward_type="prob_diff", max_iter=50, max_words_changed=5
-    ):
+    def __init__(self, model, transformation, constraints=[], num_iter=1000, max_words_changed=5):
         super().__init__(model, transformation, constraints=constraints)
-        self.reward_type = reward_type
-        self.max_iter = max_iter
+
+        # MCTS Hyper-parameters
+        self.num_iter = num_iter
         self.max_words_changed = max_words_changed
-        self.bestfeature = []
         self.ucb_C = 2
 
 
+    def generate_transformations(self, tokenized_text, indices_to_replace):
+        # Uses `get_transformations` and add item to TransformationCache
+        transformed_texts = self.get_transformations(
+            tokenized_text,
+            original_text=self.tree.original_text,
+            indices_to_replace=indices_to_replace
+        )
+        transformed_words = [text.attack_attrs['new_word'] for text in transformed_texts]
+
+        self
+
+
     def choose_random_action(self):
-        return random.choice(tuple(self.tree.unchanged_words))
-
-
-    # def play_action(self, current_text, action):
-    #     """
-    #     Generate transformation for the chosen word of current text and choose the best one.
-
-    #     Args:
-    #         action: index of the word to transform
-
-    #     Returns: TokenizedText object, list of scores
-    #     """
-
-    #     transformations = self.get_transformations(current_text, indices_to_replace=[action])
-    #     if len(transformations) != 0:
-    #         scores = self._call_model(transformations)
-    #         best_index = scores[:, self.tree.original_label].argmin()
-    #         return transformations[best_index], scores[best_index]
-    #     else:
-    #         return current_text, None
+        if self.tree.available_actions:
+            return random.choice(tuple(self.tree.available_actions))
+        else:
+            return None
 
     def evaluate_transformation(self, transformed_text):
         prob_scores = self._call_model([transformed_text])[0]
@@ -121,6 +177,7 @@ class MonteCarloTreeSearch(Attack):
         while not self.tree.terminate:
             action = self.choose_random_action()
             current_text, new_label, value = self.evaluate_action(current_text, action)
+            self.tree.available_actions.remove(action)
 
             if new_label != self.tree.original_label:
                 self.tree.terminate = True
@@ -129,18 +186,17 @@ class MonteCarloTreeSearch(Attack):
                 self.tree.terminate = True
                 self.game_value = value
 
-    def expansion(self, current_node, current_text):
-        if current_node.depth < self.tree.max_depth:
-            action = self.choose_random_action()
-            current_node.children[action] = Node(current_node.depth+1, current_node)
-            current_node = current_node.children[action]
-            self.tree.unchanged_words.remove(action)
+    def expansion(self, current_node):
+        action = self.choose_random_action()
+        current_node.children[action] = Node(current_node.depth+1, current_node)
+        current_node = current_node.children[action]
+        self.tree.available_actions.remove(action)
 
-            current_text, new_label, value = self.evaluate_action(current_text, action)
+        current_text, new_label, value = self.evaluate_action(current_text, action)
 
-            if new_label != self.tree.original_label:
-                self.tree.terminate = True
-                self.game_value = value
+        if new_label != self.tree.original_label:
+            self.tree.terminate = True
+            self.game_value = value
 
         else:
             self.tree.terminate = True
@@ -152,46 +208,43 @@ class MonteCarloTreeSearch(Attack):
     def UCB(self, node, parent_num_visits):
         return node.value + math.sqrt(self.ucb_C * math.log(parent_num_visits) / node.num_visits)
 
+    def UCB_RAVE_tuned(self, node, action, parent_num_visits):
+        ucb = self.ucb_C * math.log(parent_num_visits) / node.num_visits
+        return node.value + self.tree.global_rave_values[action] + self.node.local_rave_values[action]
+                + math.sqrt(ucb * min(0.25, node.variance + math.sqrt(ucb)))
+    
     def selection(self):
 
         current_node = self.tree.root
-        current_text = self.tree.original_text
+        current_text = self.tree.root.state
+
         best_next_node = None
         best_ucb_value = float('-inf')
         best_action = None
 
-        while not (current_node.is_leaf() or self.tree.terminate):
+        while not current_node.is_leaf():
 
             for action in current_node.children.keys():
-                ucb_value = self.UCB(current_node.children[action], current_node.num_visits)
-
+                ucb_value = self.UCB_RAVE_tuned(current_node.children[action], action, current_node.num_visits)
+ 
                 if ucb_value > best_ucb_value:
                     best_next_node = current_node.children[action]
                     best_ucb_value = ucb_value
                     best_action = action
 
             current_node = best_next_node
+            self.tree.available_actions.remove(best_action)
 
-            # Visiting the current node
-            current_text, new_label, value = self.evaluate_action(current_text, best_action)
-            self.tree.unchanged_words.remove(best_action)
+        return current_node
 
-            if new_label != self.tree.original_label:
-                self.tree.terminate = True
-                self.tree.game_value = value
+    def run_mcts(self):
 
-        return current_node, current_text
-
-    def run_mcts(self, original_label, original_score, tokenized_text):
-
-        self.tree = SearchTree(tokenized_text, original_label, original_score, self.max_words_changed)
-
-        for i in range(self.max_iter):
+        for i in range(self.num_iter):
             self.tree.reset_tree()
-            current_node, current_text = self.selection()
+            current_node = self.selection()
             
-            if not self.tree.terminate:
-                current_node, current_text = self.expansion(current_node, current_text)
+            if current_node.depth < 5:
+                current_node = self.expansion(current_node)
 
             if not self.tree.terminate:
                 self.simulate(current_node, current_text)
@@ -212,17 +265,40 @@ class MonteCarloTreeSearch(Attack):
     def attack_one(self, original_label, tokenized_text):
 
         original_tokenized_text = tokenized_text
-        original_prob = self._call_model([original_tokenized_text]).squeeze()
-        original_score = original_prob[original_label]
-        max_words_changed = min(self.max_words_changed, len(tokenized_text.words))
+        original_confidence = self._call_model([original_tokenized_text]).squeeze()[original_label]
+        max_tree_depth = min(self.max_words_changed, len(tokenized_text.words))
 
+        self.tree = SearchTree(tokenized_text, original_label, original_confidence, max_tree_depth)
+
+        replacements = self.transformation._get_replacement_words(tokenized_text.words[2])
+
+        print(replacements)
+
+        profiler = cProfile.Profile()
+        profiler.enable()
+        replacements = self.get_transformations(tokenized_text, indices_to_replace=[0,1,2,3,4])
+        profiler.disable()
+
+        s = io.StringIO()
+        sortby = SortKey.CUMULATIVE
+        ps = pstats.Stats(profiler, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+
+        """
         for i in range(max_words_changed):
-            best_next_action = self.run_mcts(original_label, original_score, tokenized_text)
+            best_next_action = self.run_mcts(
+                                original_label, 
+                                original_confidence_score, 
+                                tokenized_text,
+                                max_words_changed
+                            )
 
             tokenized_text, new_label, _ = self.evaluate_action(tokenized_text, best_next_action)
 
             if new_label != original_label:
                 break
+        """
 
         if original_label == new_label:
             return FailedAttackResult(original_tokenized_text, original_label)
