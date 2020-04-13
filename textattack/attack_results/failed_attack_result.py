8c2ea7f6d6112c73143a6773e3d3ac64f18a2a24
Jack Morris <jxmorris12@gmail.com>
2020-04-13 19:29:47 -0400

    failed attack result fix

--- textattack/attack_results/failed_attack_result.py
+++ textattack/attack_results/failed_attack_result.py
@@ -2,8 +2,10 @@ from textattack.attack_results import AttackResult
 from textattack.shared import utils
 
 class FailedAttackResult(AttackResult):
-    def __init__(self, original_text, original_output):
-        super().__init__(original_text, original_text, original_output, original_output)
+    def __init__(self, original_text, original_output, perturbed_text=None, perturbed_output=None):
+        perturbed_text = perturbed_text or original_text
+        perturbed_output = perturbed_output or original_output
+        super().__init__(original_text, perturbed_text, original_output, perturbed_output)
 
     def __data__(self, color_method=None):
         data = (self.result_str(color_method), self.original_text.text)
