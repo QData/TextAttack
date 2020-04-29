73dea51b3589f9453691330f1595c8773fdae9a2
Jack Morris <jxmorris12@gmail.com>
2020-04-29 13:43:09 -0400

    change test outputs to sample_outputs/ so that it shows up on git

--- local_tests/command_line_tests.py
+++ local_tests/command_line_tests.py
@@ -22,7 +22,7 @@ def register_test(command, name=None, output_file=None, desc=None):
 #
 register_test('python scripts/run_attack.py --model bert-mr --recipe textfooler --num_examples 10', 
     name='run_attack_textfooler_bert_mr_10', 
-    output_file='local_tests/outputs/run_attack_textfooler_bert_mr_10.txt', 
+    output_file='local_tests/sample_outputs/run_attack_textfooler_bert_mr_10.txt', 
     desc='Runs attack using TextFooler recipe on BERT using 10 examples from the MR dataset') 
 
 #
@@ -31,7 +31,7 @@ register_test('python scripts/run_attack.py --model bert-mr --recipe textfooler 
 #
 register_test('python scripts/run_attack.py --model bert-snli --recipe textfooler --num_examples 10', 
     name='run_attack_textfooler_bert_snli_10', 
-    output_file='local_tests/outputs/run_attack_textfooler_bert_snli_10.txt', 
+    output_file='local_tests/sample_outputs/run_attack_textfooler_bert_snli_10.txt', 
     desc='Runs attack using TextFooler recipe on BERT using 10 examples from the SNLI dataset')
     
 #
@@ -40,7 +40,7 @@ register_test('python scripts/run_attack.py --model bert-snli --recipe textfoole
 #
 register_test('python scripts/run_attack.py --model lstm-mr --recipe deepwordbug --num_examples 10', 
     name='run_attack_deepwordbug_lstm_mr_10', 
-    output_file='local_tests/outputs/run_attack_deepwordbug_lstm_mr_10.txt', 
+    output_file='local_tests/sample_outputs/run_attack_deepwordbug_lstm_mr_10.txt', 
     desc='Runs attack using DeepWordBUG recipe on LSTM using 10 examples from the MR dataset')
     
 #
@@ -53,7 +53,7 @@ register_test(('python scripts/run_attack.py --attack_n --goal_function targeted
     '--enable_csv --model bert-mnli --num_examples 10 --transformation word-swap-wordnet '
     '--constraints lang-tool --attack beam-search:beam_width=2'), 
     name='run_attack_targeted2_bertmnli_wordnet_beamwidth_2_enablecsv_attackn', 
-    output_file='local_tests/outputs/run_attack_targetedclassification2_wordnet_langtool_enable_csv_beamsearch2_attack_n_10.txt', 
+    output_file='local_tests/sample_outputs/run_attack_targetedclassification2_wordnet_langtool_enable_csv_beamsearch2_attack_n_10.txt', 
     desc=('Runs attack using targeted classification on class 2 on BERT MNLI with'
         'enable_csv and attack_n set, using the WordNet transformation and beam '
         'search with  beam width 2, using language tool constraint, on 10 samples')
