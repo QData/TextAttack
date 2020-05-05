2c91f7ee936729e81cc30912a9803dbe2387b7a9
Jack Morris <jxmorris12@gmail.com>
2020-05-04 20:42:03 -0400

    display table in w&b log

--- textattack/loggers/weights_and_biases_logger.py
+++ textattack/loggers/weights_and_biases_logger.py
@@ -1,22 +1,33 @@
 import wandb
 
+from textattack.shared.utils import html_table_from_rows
 from .logger import Logger
 
 class WeightsAndBiasesLogger(Logger):
     def __init__(self, filename='', stdout=False):
-        wandb.init()
-
+        wandb.init(project='textattack')
+        self._result_table_rows = []
+        
+    def _log_result_table(self):
+        """ Weights & Biases doesn't have a feature to automatically 
+            aggregate results across timesteps and display the full table.
+            Therefore, we have to do it manually.
+        """
+        result_table = html_table_from_rows(self._result_table_rows)
+        wandb.log({ 'results': wandb.Html(result_table) })
+        
     def log_attack_result(self, result):
         original_text_colored, perturbed_text_colored = result.diff_color(color_method='html')
-        original_text_colored = wandb.Html(original_text_colored)
-        perturbed_text_colored = wandb.Html(perturbed_text_colored)
+        self._result_table_rows.append([original_text_colored, perturbed_text_colored])
+        result_diff_table = html_table_from_rows([[original_text_colored, perturbed_text_colored]])
+        result_diff_table = wandb.Html(result_diff_table)
         wandb.log({
-            'type': type(result).__name__,
-            'original_text': original_text_colored,
-            'perturbed_text': perturbed_text_colored,
+            'resultType': type(result).__name__,
+            'result': result_diff_table,
             'original_output': result.original_result.output,
             'perturbed_output': result.perturbed_result.output,
         })
+        self._log_result_table()
 
     def log_summary_rows(self, rows, title, window_id):
         print('w&b skipping summary')
