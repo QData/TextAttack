480a9308f98dac149ad7d9c2ed8c65f38db85085
Jack Morris <jxmorris12@gmail.com>
2020-05-13 13:41:03 -0400

    add summary statistics

--- textattack/loggers/weights_and_biases_logger.py
+++ textattack/loggers/weights_and_biases_logger.py
@@ -7,14 +7,23 @@ class WeightsAndBiasesLogger(Logger):
     def __init__(self, filename='', stdout=False):
         wandb.init(project='textattack')
         self._result_table_rows = []
+
+    def log_summary_rows(self, rows, title, window_id):
+        table = wandb.Table(columns=['Attack Results', ''])
+        for row in rows:
+            table.add_data(*row)
+            metric_name, metric_score = row
+            wandb.run.summary[metric_name] = metric_score
+        wandb.log({ 'attack_params': table })
         
     def _log_result_table(self):
         """ Weights & Biases doesn't have a feature to automatically 
             aggregate results across timesteps and display the full table.
             Therefore, we have to do it manually.
         """
-        result_table = html_table_from_rows(self._result_table_rows)
-        wandb.log({ 'results': wandb.Html(result_table, inject=False) })
+        result_table = html_table_from_rows(self._result_table_rows,
+            header=['', 'Original Input', 'Perturbed Input'])
+        wandb.log({ 'results': wandb.Html(result_table) })
         
     def log_attack_result(self, result):
         original_text_colored, perturbed_text_colored = result.diff_color(color_method='html')
@@ -23,18 +32,11 @@ class WeightsAndBiasesLogger(Logger):
         result_diff_table = html_table_from_rows([[original_text_colored, perturbed_text_colored]])
         result_diff_table = wandb.Html(result_diff_table)
         wandb.log({
-            'resultType': type(result).__name__,
             'result': result_diff_table,
             'original_output': result.original_result.output,
             'perturbed_output': result.perturbed_result.output,
         })
         self._log_result_table()
-
-    def log_summary_rows(self, rows, title, window_id):
-        print('w&b skipping summary')
-        pass
-        # @TODO: should we log some summary to W&B? It seems to automatically
-        # calculate its own summary statistics.
 
     def log_sep(self):
         self.fout.write('-' * 90 + '\n')
