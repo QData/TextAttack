f0f40826a8b8c88d1033b6a273aaf06ee778bc61
Jack Morris <jxmorris12@gmail.com>
2020-06-05 12:01:51 -0400

    bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/QData/textattack",
-    packages=setuptools.find_namespace_packages(),
+    packages=setuptools.find_packages(exclude=['wandb']),
     package_data={
      '': ['config.json'],
     },
