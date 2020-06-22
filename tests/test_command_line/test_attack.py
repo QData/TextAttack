3836386b2f10de4e233df6bcde1c35905c5a8009
Jack Morris <jxmorris12@gmail.com>
2020-06-21 22:22:47 -0400

    formatting update

--- tests/test_command_line/test_attack.py
+++ tests/test_command_line/test_attack.py
@@ -126,7 +126,11 @@ def test_command_line_attack(name, command, sample_output_file):
     print("desired_output =>", desired_output)
     # regex in sample file look like /.*/
     # / is escaped in python 3.6, but not 3.7+, so we support both
-    desired_re = re.escape(desired_output).replace("/\\.\\*/", ".*").replace("\\/\\.\\*\\/", ".*")
+    desired_re = (
+        re.escape(desired_output)
+        .replace("/\\.\\*/", ".*")
+        .replace("\\/\\.\\*\\/", ".*")
+    )
     result = run_command_and_get_result(command)
     # get output and check match
     assert result.stdout is not None
