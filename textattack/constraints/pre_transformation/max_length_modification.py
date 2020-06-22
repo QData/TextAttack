c41510343924237ccdc9499b1e68657e78d230e9
Jin Yong Yoo <jy2ma@virginia.edu>
2020-06-21 22:02:08 -0400

    fix typo

--- textattack/constraints/pre_transformation/max_length_modification.py
+++ textattack/constraints/pre_transformation/max_length_modification.py
@@ -8,7 +8,7 @@ class MaxLengthModification(PreTransformationConstraint):
     """
 
     def __init__(self, max_length):
-        self.max_length
+        self.max_length = max_length
 
     def _get_modifiable_indices(self, current_text):
         """ Returns the word indices in current_text which are able to be deleted """
