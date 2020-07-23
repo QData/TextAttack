e6e16a3fee6e8f75a9e04bb78d6960ad18621c5c
Jack Morris <jxmorris12@gmail.com>
2020-07-23 15:45:29 -0400

    release v0.2.2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, UVA QData Lab"
 author = "UVA QData Lab"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.1"
+release = "0.2.2"
 
 # Set master doc to `index.rst`.
 master_doc = "index"
