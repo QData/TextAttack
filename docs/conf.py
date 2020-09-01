02c9d76cadc85abd2a5203ed9dec334c4c73679a
Jack Morris <jxmorris12@gmail.com>
2020-08-31 21:37:35 -0400

    bump flair & textattack versions

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, UVA QData Lab"
 author = "UVA QData Lab"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.8"
+release = "0.2.9"
 
 # Set master doc to `index.rst`.
 master_doc = "index"
