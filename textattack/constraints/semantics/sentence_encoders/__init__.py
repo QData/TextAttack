d5b3e4b7c12a769a61a37aa37a339bf414b627a0
Florian Over <over@axxessio.com>
2020-10-11 23:46:28 +0200

    Fixed init file

--- textattack/constraints/semantics/sentence_encoders/__init__.py
+++ textattack/constraints/semantics/sentence_encoders/__init__.py
@@ -3,7 +3,4 @@ from .sentence_encoder import SentenceEncoder
 from .bert import BERT
 from .infer_sent import InferSent
 from .thought_vector import ThoughtVector
-from .universal_sentence_encoder import UniversalSentenceEncoder
-from .multilingual_universal_sentence_encoder import (
-    MultilingualUniversalSentenceEncoder,
-)
+from .universal_sentence_encoder import UniversalSentenceEncoder, MultilingualUniversalSentenceEncoder
