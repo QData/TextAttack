ae1c1f07a90e958fc666736a1883e95253013d69
Arshdeep Sekhon <as5cu@virginia.edu>
2020-11-04 07:59:22 -0500

    load user dataset from local files and convert to TextAttack dataset using hugging face data loaders

--- textattack/datasets/__init__.py
+++ textattack/datasets/__init__.py
@@ -10,6 +10,5 @@ TextAttack allows users to provide their own dataset or load from HuggingFace.
 
 from .dataset import TextAttackDataset
 from .huggingface_dataset import HuggingFaceDataset
-from .huggingface_dataset import CustomDataset
-
+from .custom_dataset import CustomDataset
 from . import translation
