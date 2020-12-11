9f771927f48ff2e534a432be24e451dc3ceebe3a
Derek Chia <derek@derekchia.com>
2020-12-11 13:35:01 +0800

    fix bug during random_swap

--- textattack/augmentation/augmenter.py
+++ textattack/augmentation/augmenter.py
@@ -96,7 +96,7 @@ class Augmenter:
                     transformed_texts, current_text, original_text
                 )
 
-                # if there's no more transformed texts after filter or that transformed texts only contain duplicated words, or there is no synonym to insert , terminate
+                # if there's no more transformed texts after filter or that transformed texts only contain duplicated words, or there is no synonym to insert, terminate
                 if not len(transformed_texts) or len(set(transformed_texts)) == 1 or transformed_texts == [current_text]:
                     break
 
