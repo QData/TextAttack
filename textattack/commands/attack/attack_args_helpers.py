026e20c6b90cb0c0b12c73c7bd74ce77cbed1e93
MintForever <lin_xinyue@outlook.com>
2021-01-05 18:25:24 -0500

    fix error

--- textattack/commands/attack/attack_args_helpers.py
+++ textattack/commands/attack/attack_args_helpers.py
@@ -423,11 +423,16 @@ def parse_dataset_from_args(args):
                 dataset_args = (dataset_args,)
         if args.split:
             dataset_try = list(dataset_args)
-            dataset_try[2] = args.split
-            dataset = textattack.datasets.HuggingFaceDataset(
-                *dataset_try,
-                shuffle=args.shuffle
-            )
+            if len(dataset_try) > 2:
+                dataset_try[2] = args.split
+                dataset = textattack.datasets.HuggingFaceDataset(
+                    *dataset_try, shuffle=args.shuffle
+                )
+            else:
+                dataset = textattack.datasets.HuggingFaceDataset(
+                    *dataset_try, shuffle=args.shuffle, split=args.split
+                )
+
         else:
             dataset = textattack.datasets.HuggingFaceDataset(
                 *dataset_args, shuffle=args.shuffle
