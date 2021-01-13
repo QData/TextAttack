7f0fd5ef3232f40e0f8b053f37b06d18fc34b9d6
diegoc <diegoccy@users.noreply.github.com>
2021-01-13 13:50:57 +0800

    Attempt 2
    Removed changes made previously from the following file:
    attack_result.py
    
    Added changes to the following file:
    filelogger.py
    
    Implemented nocolor as a class attribute instead of global variable

--- textattack/loggers/file_logger.py
+++ textattack/loggers/file_logger.py
@@ -14,6 +14,8 @@ from .logger import Logger
 class FileLogger(Logger):
     """Logs the results of an attack to a file, or `stdout`."""
 
+    nocolor = False
+
     def __init__(self, filename="", stdout=False):
         self.stdout = stdout
         self.filename = filename
@@ -43,6 +45,8 @@ class FileLogger(Logger):
     def log_attack_result(self, result):
         self.num_results += 1
         color_method = "ansi" if self.stdout else "file"
+        if self.nocolor:
+            color_method = None
         self.fout.write(
             "-" * 45 + " Result " + str(self.num_results) + " " + "-" * 45 + "\n"
         )
