facad249d8c55e5f6cefa1f1bd82783971f05040
Jack Morris <jxmorris12@gmail.com>
2021-05-23 19:44:35 -0400

    args.global_step -> global_step

--- textattack/commands/train_model/run_training.py
+++ textattack/commands/train_model/run_training.py
@@ -552,7 +552,7 @@ def train_model(args):
             tb_writer.add_scalar("epoch_eval_score", eval_score, epoch)
 
             if args.checkpoint_every_epoch:
-                _save_model_checkpoint(model, args.output_dir, args.global_step)
+                _save_model_checkpoint(model, args.output_dir, global_step)
 
             logger.info(
                 f"Eval {'pearson correlation' if args.do_regression else 'accuracy'}: {eval_score*100}%"
