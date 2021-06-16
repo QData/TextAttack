8fce0db7ad4fc6b6378f7b2751b7943d48a19183
Matrix <32097922+matrix1001@users.noreply.github.com>
2021-06-16 08:40:28 +0800

    Update attacked_text.py
    add words_diff_num

--- textattack/shared/attacked_text.py
+++ textattack/shared/attacked_text.py
@@ -237,22 +237,9 @@ class AttackedText:
         indices = set()
         w1 = self.words
         w2 = other_attacked_text.words
-        idx1 = 0
-        idx2 = 0
-        flag = False
-        while (idx1 < len(w1) and idx2 < len(w2)):
-            if w1[idx1] == w2[idx2]:
-                flag = False
-                idx1 += 1
-                idx2 += 1
-            elif flag == False:
-                flag = True
-                indices.add(idx1)
-                idx1 += 1
-                idx2 += 1
-            else:
-                flag = False
-                idx2 += 1
+        for i in range(min(len(w1), len(w2))):
+            if w1[i] != w2[i]:
+                indices.add(i)
         return indices
 
     def ith_word_diff(self, other_attacked_text, i):
@@ -263,6 +250,43 @@ class AttackedText:
         if len(w1) - 1 < i or len(w2) - 1 < i:
             return True
         return w1[i] != w2[i]
+    
+    def words_diff_num(self, other_attacked_text):
+        # using edit distance to calculate words diff num
+        def generate_tokens(words):
+            result = {}
+            idx = 1
+            for w in words:
+                if w not in result:
+                    result[w] = idx
+                    idx += 1
+            return result
+        def words_to_tokens(words, tokens):
+            result = []
+            for w in words:
+                result.append(tokens[w])
+            return result
+        def edit_distance(w1_t, w2_t):
+            matrix = [[i + j for j in range(len(w2_t) + 1)] for i in range(len(w1_t) + 1)]
+
+            for i in range(1, len(w1_t) + 1):
+                for j in range(1, len(w2_t) + 1):
+                    if w1_t[i - 1] == w2_t[j - 1]:
+                        d = 0
+                    else:
+                        d = 1
+                    matrix[i][j] = min(matrix[i - 1][j] + 1, matrix[i][j - 1] + 1, matrix[i - 1][j - 1] + d)
+
+            return matrix[len(w1_t)][len(w2_t)]
+        def cal_dif(w1, w2):
+            tokens = generate_tokens(w1 + w2)
+            w1_t = words_to_tokens(w1, tokens)
+            w2_t = words_to_tokens(w2, tokens)
+            return edit_distance(w1_t, w2_t)
+        w1 = self.words
+        w2 = other_attacked_text.words
+        return cal_dif(w1, w2)
+
 
     def convert_from_original_idxs(self, idxs):
         """Takes indices of words from original string and converts them to
