90cc0ce851a0be7e0b25b31d304c1f982cffb1a2
ss7mu <ss7mu@qdev3.cs.Virginia.EDU>
2021-06-24 15:16:01 -0400

    Rename metrics to attack_metrics

---
+++ textattack/attack_metrics/__init__.py
@@ -0,0 +1,11 @@
+"""
+
+attack_metrics:
+======================
+
+TextAttack allows users to use their own metrics on adversarial examples or select common metrics to display.
+
+
+"""
+
+from .attack_metrics import AttackMetrics
