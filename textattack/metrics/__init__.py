5a882536925e0d60231c4d75b8e67e9a77962575
sanchit97 <sanchit15083@iiitd.ac.in>
2021-08-20 02:18:43 -0400

    [CODE] Fixing init file

--- textattack/metrics/__init__.py
+++ textattack/metrics/__init__.py
@@ -2,7 +2,9 @@
 """
 
 from .metric import Metric
+
 from .attack_metrics import AttackSuccessRate
 from .attack_metrics import WordsPerturbed
 from .attack_metrics import AttackQueries
-# from .quality_metrics import QualityMetric
+
+from .quality_metrics import Perplexity
