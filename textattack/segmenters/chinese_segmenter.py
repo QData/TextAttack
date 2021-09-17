7eaf0823e1051d1075c261bd2517d0f7a15b4cb3
diegoc <diegoccy@users.noreply.github.com>
2021-09-17 12:34:15 -0400

    Add new homophone transformation
    - added homophone transformation
    - added Chinese character segmentation

--- textattack/segmenters/chinese_segmenter.py
+++ textattack/segmenters/chinese_segmenter.py
@@ -2,7 +2,11 @@ from pywordseg import Wordseg
 
 
 class ChineseSegmenter:
-    def segment_data(self, data):
+    def segment_by_word(self, data):
+        """
+        segments data by chinese words
+        https://pypi.org/project/pywordseg/
+        """
         seg = Wordseg(
             batch_size=64,
             device="cpu",
@@ -20,4 +24,23 @@ class ChineseSegmenter:
                 processed = processed + segmented[i][j] + " "
         data["text"] = processed
 
+        return data
+
+    def segment_by_character(self, data):
+        """segments data by chinese characters"""
+
+        segmented_data = []
+        for sentence in data["text"]:
+            segmented_sentence = []
+            for character in sentence:
+                segmented_sentence.append(character)
+            segmented_data.append(segmented_sentence)
+
+        processed = ""
+
+        for i in range(0, len(segmented_data)):
+            for j in range(0, len(segmented_data[i])):
+                processed = processed + segmented_data[i][j] + " "
+
+        data["text"] = processed
         return data
---
+++ textattack/transformations/word_swaps/chinese_homophone_character_swap.py
@@ -0,0 +1,30 @@
+import pinyin
+import pandas as pd
+from .word_swap import WordSwap
+
+class ChineseHomophoneCharacterSwap(WordSwap):
+    """Transforms an input by replacing its words with synonyms provided by
+    WordNet."""
+
+    def __init__(self):
+        homophone_dict = pd.read_csv('chinese_homophone_char.txt', header=None, sep='\n')
+
+        homophone_dict = homophone_dict[0].str.split('\t', expand=True)
+
+        self.homophone_dict = homophone_dict
+
+    def _get_replacement_words(self, word):
+        """Returns a list containing all possible words with 1 character
+        replaced by a homoglyph."""
+        homophones = []
+
+        word = pinyin.get(word, format="strip", delimiter=" ")
+        if word in self.homophone_dict.values:
+            for row in range(self.homophone_dict.shape[0]):  # df is the DataFrame
+                for col in range(0, 1):
+                    if self.homophone_dict._get_value(row, col) == word:
+                        for i in range(1, 4):
+                            homophones.append(self.homophone_dict[col + i][row])
+            return homophones
+        else:
+            return []
