8cf9dd4cd565d89cc1d9b2743b066887e3c563f7
diegoc <diegoccy@users.noreply.github.com>
2021-10-01 05:33:01 -0400

    Add sentence level transformation: back-translation

---
+++ textattack/transformations/sentence_transformations/sentence_transformation.py
@@ -0,0 +1,7 @@
+"https://github.com/makcedward/nlpaug"
+
+from textattack.transformations import Transformation
+
+class SentenceTransformation(Transformation):
+    def _get_transformations(self, current_text, indices_to_modify):
+        raise NotImplementedError()
