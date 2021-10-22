8b91d7e1ce391a9a0c7a11c95e4406e65d688f3a
sanchit97 <ss7mu@virginia.edu>
2021-10-21 22:25:47 -0400

    [TEST] Renamed metric api test to push down the q

---
+++ tests/test_zmetric_api.py
@@ -0,0 +1,26 @@
+def test_perplexity():
+    import textattack
+    from textattack.attack_results import SuccessfulAttackResult
+    from textattack.goal_function_results.classification_goal_function_result import (
+        ClassificationGoalFunctionResult,
+    )
+    from textattack.shared.attacked_text import AttackedText
+
+    sample_text = "hide new secretions from the parental units "
+    sample_atck_text = "Ehide enw secretions from the parental units "
+
+    results = [
+        SuccessfulAttackResult(
+            ClassificationGoalFunctionResult(
+                AttackedText(sample_text), None, None, None, None, None, None
+            ),
+            ClassificationGoalFunctionResult(
+                AttackedText(sample_atck_text), None, None, None, None, None, None
+            ),
+        )
+    ]
+
+    ppl = textattack.metrics.quality_metrics.Perplexity().calculate(results)
+
+    assert int(ppl["avg_original_perplexity"]) == int(1854.74)
+    assert int(ppl["avg_attack_perplexity"]) == int(4214.01)
