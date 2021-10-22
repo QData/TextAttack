de153ef02aa6ee968e536026eb975bf82c4a4913
diegoc <diegoccy@users.noreply.github.com>
2021-10-22 10:25:16 -0400

    format update

--- textattack/attack_recipes/french_recipe.py
+++ textattack/attack_recipes/french_recipe.py
@@ -1,16 +1,32 @@
 from textattack import Attack
-from textattack.constraints.pre_transformation import RepeatModification, StopwordModification
+from textattack.constraints.pre_transformation import (
+    RepeatModification,
+    StopwordModification,
+)
 from textattack.goal_functions import UntargetedClassification
 from textattack.search_methods import GreedyWordSwapWIR
-from textattack.transformations import WordSwapWordNet, CompositeTransformation, WordSwapChangeLocation, WordSwapChangeName
+from textattack.transformations import (
+    CompositeTransformation,
+    WordSwapChangeLocation,
+    WordSwapChangeName,
+    WordSwapWordNet,
+)
+
 from .attack_recipe import AttackRecipe
+
 
 class French_Recipe(AttackRecipe):
     @staticmethod
     def build(model_wrapper):
-        transformation = WordSwapWordNet('fra')
-        transformation.language= 'fra'
-        transformation = CompositeTransformation([transformation, WordSwapChangeLocation(language="fra"),WordSwapChangeName(language="fra")])
+        transformation = WordSwapWordNet("fra")
+        transformation.language = "fra"
+        transformation = CompositeTransformation(
+            [
+                transformation,
+                WordSwapChangeLocation(language="fra"),
+                WordSwapChangeName(language="fra"),
+            ]
+        )
         constraints = [RepeatModification(), StopwordModification("french")]
         goal_function = UntargetedClassification(model_wrapper)
         search_method = GreedyWordSwapWIR()
