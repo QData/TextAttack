e1217f7679dfa3b361b08b9402412a41c4af75b9
sanchit97 <ss7mu@virginia.edu>
2021-10-30 11:34:41 -0400

    [FIX,DOC] New api doc, use small model, add use test

--- tests/test_metric_api.py
+++ tests/test_metric_api.py
@@ -1,9 +1,9 @@
 def test_perplexity():
-    # import textattack
     from textattack.attack_results import SuccessfulAttackResult
     from textattack.goal_function_results.classification_goal_function_result import (
         ClassificationGoalFunctionResult,
     )
+    from textattack.metrics.quality_metrics import Perplexity
     from textattack.shared.attacked_text import AttackedText
 
     sample_text = "hide new secretions from the parental units "
@@ -19,8 +19,36 @@ def test_perplexity():
             ),
         )
     ]
-    results[0]
-    # ppl = textattack.metrics.quality_metrics.Perplexity().calculate(results)
+    ppl = Perplexity(model_name="distilbert-base-uncased").calculate(results)
 
-    # assert int(ppl["avg_original_perplexity"]) == int(1854.74)
-    # assert int(ppl["avg_attack_perplexity"]) == int(4214.01)
+    assert int(ppl["avg_original_perplexity"]) == int(81.95)
+
+
+def test_use():
+    import transformers
+
+    import textattack
+    from textattack.metrics.quality_metrics import USEMetric
+
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(
+        "distilbert-base-uncased-finetuned-sst-2-english"
+    )
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        "distilbert-base-uncased-finetuned-sst-2-english"
+    )
+    model_wrapper = textattack.models.wrappers.HuggingFaceModelWrapper(model, tokenizer)
+    attack = textattack.attack_recipes.DeepWordBugGao2018.build(model_wrapper)
+    dataset = textattack.datasets.HuggingFaceDataset("glue", "sst2", split="train")
+    attack_args = textattack.AttackArgs(
+        num_examples=1,
+        log_to_csv="log.csv",
+        checkpoint_interval=5,
+        checkpoint_dir="checkpoints",
+        disable_stdout=True,
+    )
+    attacker = textattack.Attacker(attack, dataset, attack_args)
+    results = attacker.attack_dataset()
+
+    usem = USEMetric().calculate(results)
+
+    assert usem["avg_attack_use_score"] == 0.76
