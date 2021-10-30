940cf722d4cee06493c115ccc49095d1257b5a8f
sanchit97 <ss7mu@virginia.edu>
2021-10-30 12:50:15 -0400

    [FIX] Set tfestimator

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ extras["tensorflow"] = [
     "tensorflow_hub",
     "tensorflow_text>=2",
     "tensorboardX",
-    "tensorflow-estimator==2.6.0"
+    "tensorflow-estimator==2.6.0",
 ]
 
 extras["optional"] = [
