44f0f100f37a1287c83588a8e1d5d417bc355174
Yanjun Qi <qiyanjun2012@gmail.com>
2021-11-09 20:34:04 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = "2020, UVA QData Lab"
 author = "UVA QData Lab"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.3"
+release = "0.3.4"
 
 # Set master doc to `index.rst`.
 master_doc = "index"
