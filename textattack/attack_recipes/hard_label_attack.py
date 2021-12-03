4174fa9cba35d9a405a464dd692f2c10f17385f5
ankitgv0 <ankitguptav0@gmail.com>
2021-12-03 18:31:57 -0500

    added hard label goal function

---
+++ textattack/attack_recipes/hard_label_attack.py
@@ -0,0 +1,87 @@
+"""
+
+Hard Label Attack Algorithm
+=======================================
+Uses the Hard Label Goal Function
+
+"""
+
+from textattack import Attack
+from textattack.constraints.grammaticality.language_models import (
+    Google1BillionWordsLanguageModel,
+)
+from textattack.constraints.overlap import MaxWordsPerturbed
+from textattack.constraints.pre_transformation import (
+    InputColumnModification,
+    RepeatModification,
+    StopwordModification,
+)
+from textattack.constraints.semantics import WordEmbeddingDistance
+from textattack.goal_functions import HardLabelClassification
+from textattack.search_methods import AlzantotGeneticAlgorithm
+from textattack.transformations import WordSwapEmbedding
+
+from .attack_recipe import AttackRecipe
+
+
+class HardLabelAttack(AttackRecipe):
+    """Alzantot, M., Sharma, Y., Elgohary, A., Ho, B., Srivastava, M.B., &
+    Chang, K. (2018).
+
+    Generating Natural Language Adversarial Examples.
+
+    https://arxiv.org/abs/1804.07998
+    """
+
+    @staticmethod
+    def build(model_wrapper):
+        #
+        # Swap words with their embedding nearest-neighbors.
+        #
+        # Embedding: Counter-fitted Paragram Embeddings.
+        #
+        # "[We] fix the hyperparameter values to S = 60, N = 8, K = 4, and δ = 0.5"
+        #
+        transformation = WordSwapEmbedding(max_candidates=8)
+        #
+        # Don't modify the same word twice or stopwords
+        #
+        constraints = [RepeatModification(), StopwordModification()]
+        #
+        # During entailment, we should only edit the hypothesis - keep the premise
+        # the same.
+        #
+        input_column_modification = InputColumnModification(
+            ["premise", "hypothesis"], {"premise"}
+        )
+        constraints.append(input_column_modification)
+        #
+        # Maximum words perturbed percentage of 20%
+        #
+        constraints.append(MaxWordsPerturbed(max_percent=0.2))
+        #
+        # Maximum word embedding euclidean distance of 0.5.
+        #
+        constraints.append(
+            WordEmbeddingDistance(max_mse_dist=0.5, compare_against_original=False)
+        )
+        #
+        # Language Model
+        #
+        constraints.append(
+            Google1BillionWordsLanguageModel(
+                top_n_per_index=4, compare_against_original=False
+            )
+        )
+        #
+        # Goal is hard label classification
+        #
+        goal_function = HardLabelClassification(model_wrapper)
+        #
+        # Perform word substitution with a genetic algorithm.
+        #
+        search_method = AlzantotGeneticAlgorithm(
+            pop_size=60, max_iters=20, post_crossover_check=False
+        )
+
+        return Attack(goal_function, constraints, transformation, search_method)
---
+++ textattack/goal_functions/classification/hardlabel_classification.py
@@ -0,0 +1,41 @@
+"""
+
+Determine successful in untargeted Classification
+----------------------------------------------------
+"""
+
+
+from .classification_goal_function import ClassificationGoalFunction
+
+
+class HardLabelClassification(ClassificationGoalFunction):
+    """An untargeted attack on classification models which attempts to minimize
+    the score of the correct label until it is no longer the predicted label.
+
+    Args:
+        target_max_score (float): If set, goal is to reduce model output to
+            below this score. Otherwise, goal is to change the overall predicted
+            class.
+    """
+
+    def __init__(self, *args, target_max_score=None, **kwargs):
+        self.target_max_score = target_max_score
+        super().__init__(*args, **kwargs)
+
+    def _is_goal_complete(self, model_output, _):
+        if self.target_max_score:
+            return model_output[self.ground_truth_output] < self.target_max_score
+        elif (model_output.numel() == 1) and isinstance(
+            self.ground_truth_output, float
+        ):
+            return abs(self.ground_truth_output - model_output.item()) >= 0.5
+        else:
+            return model_output.argmax() != self.ground_truth_output
+
+    def _get_score(self, model_output, _):
+        # If the model outputs a single number and the ground truth output is
+        # a float, we assume that this is a regression task.
+        if (model_output.numel() == 1) and isinstance(self.ground_truth_output, float):
+            return max(model_output.item(), self.ground_truth_output)
+        else:
+            return 1 - model_output[self.ground_truth_output]
