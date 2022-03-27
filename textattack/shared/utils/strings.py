edaa644fd37a0fd9494d57207b37fcb2fcf29ed8
diegoc <cc4fy@virginia.edu>
2022-03-27 00:11:20 -0400

    Change redundant coding

--- textattack/shared/utils/strings.py
+++ textattack/shared/utils/strings.py
@@ -88,34 +88,21 @@ def words_from_text(s, words_to_ignore=[]):
                 s = " ".join(seg_list)
             else:
                 s = " ".join(s.split())
-        for c in s:
-            if c.isalnum() or c in homos:
-                word += c
-            elif c in "'-_*@" and len(word) > 0:
-                # Allow apostrophes, hyphens, underscores, asterisks and at signs as long as they don't begin the
-                # word.
-                word += c
-            elif word:
-                if word not in words_to_ignore:
-                    words.append(word)
-                word = ""
-        if len(word) and (word not in words_to_ignore):
-            words.append(word)
     except Exception:
-        for c in " ".join(s.split()):
-            if c.isalnum() or c in homos:
-                word += c
-            elif c in "'-_*@" and len(word) > 0:
-                # Allow apostrophes, hyphens, underscores, asterisks and at signs as long as they don't begin the
-                # word.
-                word += c
-            elif word:
-                if word not in words_to_ignore:
-                    words.append(word)
-                word = ""
-        if len(word) and (word not in words_to_ignore):
-            words.append(word)
-
+        s = " ".join(s.split())
+    for c in s:
+        if c.isalnum() or c in homos:
+            word += c
+        elif c in "'-_*@" and len(word) > 0:
+            # Allow apostrophes, hyphens, underscores, asterisks and at signs as long as they don't begin the
+            # word.
+            word += c
+        elif word:
+            if word not in words_to_ignore:
+                words.append(word)
+            word = ""
+    if len(word) and (word not in words_to_ignore):
+        words.append(word)
     return words
 
 
