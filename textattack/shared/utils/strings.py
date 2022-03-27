36912d2f813d71622d3542636ee861b84d41c615
diegoc <cc4fy@virginia.edu>
2022-03-27 02:22:46 -0400

    test pytest

--- textattack/shared/utils/strings.py
+++ textattack/shared/utils/strings.py
@@ -80,16 +80,15 @@ def words_from_text(s, words_to_ignore=[]):
     words = []
     word = ""
 
-    # DetectorFactory.seed = 0
-    # try:
-    #     if s:
-    #         if detect(s) == "zh-cn" or detect(s) == "ko":
-    #             seg_list = jieba.cut(s, cut_all=False)
-    #             s = " ".join(seg_list)
-    #         else:
-    #             s = " ".join(s.split())
-    # except Exception:
-    s = " ".join(s.split())
+    try:
+        if s:
+            if detect(s) == "zh-cn" or detect(s) == "ko":
+                seg_list = jieba.cut(s, cut_all=False)
+                s = " ".join(seg_list)
+            else:
+                s = " ".join(s.split())
+    except Exception:
+        s = " ".join(s.split())
     for c in s:
         if c.isalnum() or c in homos:
             word += c
