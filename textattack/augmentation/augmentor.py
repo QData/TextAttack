562cac4f5eed29b4c7774073cf16f2195a656a8e
Hanyu Liu <hl2gn@virginia.edu>
2022-04-17 13:15:55 -0400

    cpu/gpu parallel augment

--- textattack/augmentation/augmentor.py
+++ textattack/augmentation/augmentor.py
@@ -4,6 +4,13 @@ from multiprocessing import Process, Queue, cpu_count, Pool
 import torch
 from .augmenter import Augmenter
 import os, sys
+import os
+import argparse
+import datasets
+import tqdm
+import torch
+import random
+import numpy as np
 
 
 class HiddenPrints:
@@ -18,7 +25,7 @@ class HiddenPrints:
 
 class Augmentor:
 
-    def __init__(self, augmenter, dataset, augment_args=None):
+    def __init__(self, augmenter, dataset, augment_args=None, seed=None):
 
         assert isinstance(
             augmenter, Augmenter
@@ -26,11 +33,12 @@ class Augmentor:
         self.augmenter = augmenter
         self.dataset = dataset
         self.augment_args = augment_args
+        self.seed = seed
 
     def augment(self):
         return self.augmenter.augment_many(self.dataset)
 
-    def augment_parallel(self, num_processes):
+    def augment_parallel_lazy(self, num_processes):
         # decide how many processes to start
         num_processes = min(cpu_count(), len(self.dataset), num_processes)
         print(f"Starting computations on {num_processes} cores....\n")
@@ -42,15 +50,17 @@ class Augmentor:
 
             # set up sub lists to feed to each processes
             remainder = len(dataset) % num_processes
-            result = len(dataset) // num_processes
-            num_ls = [result for _ in range(num_processes)]
+            div = len(dataset) // num_processes
+            num_ls = [div for _ in range(num_processes)]
             for i in range(remainder):
                 num_ls[i] += 1
             iter_text_list = iter(dataset)
             list_of_tx_ls = [list(islice(iter_text_list, elem)) for elem in num_ls]
 
+            '''
             for x in list_of_tx_ls:
                 print(x)
+            '''
 
             # create processes
             processes = [Process(target=augment_from_queue, args=(tx_ls, queue, self.augmenter)) for tx_ls in
@@ -78,17 +88,161 @@ class Augmentor:
 
         return result
 
+    def augment_parallel_cpu(self, num_processes):
+        augmented_text = []
+        augmented_indices = []
+        num_workers = min(cpu_count(), num_processes)
+        assert num_workers >= 1, "You need at least one GPU to perform augmentation."
+
+        """
+        torch.multiprocessing.set_start_method("spawn", force=True)
+        torch.multiprocessing.set_sharing_strategy("file_system")
+        """
+
+        in_queue = torch.multiprocessing.Queue()
+        out_queue = torch.multiprocessing.Queue()
+        for i, text in enumerate(self.dataset):
+            in_queue.put((i, text))
+
+        # Start workers.
+        worker_pool = torch.multiprocessing.Pool(
+            num_workers,
+            augment_from_queue_cpu,
+            (
+                in_queue,
+                out_queue,
+                self.augmenter
+            ),
+        )
+        pbar = tqdm.tqdm(total=len(self.dataset), smoothing=0)
+        for _ in range(len(self.dataset)):
+            idx, aug_text = out_queue.get(block=True)
+            pbar.update()
+            if isinstance(aug_text, Exception):
+                continue
+            if aug_text == "":
+                continue
+            augmented_indices.append(idx)
+            augmented_text.append(aug_text)
+
+        # Send sentinel values to worker processes
+        for _ in range(num_workers):
+            in_queue.put(("END", "END"))
+        worker_pool.terminate()
+        worker_pool.join()
+
+        augmented_indices = np.array(augmented_indices)
+        argsort_indices = np.argsort(augmented_indices)
+        augmented_text = [augmented_text[i] for i in argsort_indices]
+
+        return augmented_text
+
+    def augment_parallel_gpu(self, num_processes):
+        augmented_text = []
+        augmented_indices = []
+        num_workers = min(torch.cuda.device_count(), num_processes)
+
+        """
+        torch.multiprocessing.set_start_method("spawn", force=True)
+        torch.multiprocessing.set_sharing_strategy("file_system")
+        """
+
+        in_queue = torch.multiprocessing.Queue()
+        out_queue = torch.multiprocessing.Queue()
+        for i, text in enumerate(self.dataset):
+            in_queue.put((i, text))
+
+        # Start workers.
+        worker_pool = torch.multiprocessing.Pool(
+            num_workers,
+            augment_from_queue_gpu,
+            (
+                num_workers,
+                in_queue,
+                out_queue,
+                self.seed,
+                self.augmenter,
+            ),
+        )
+        pbar = tqdm.tqdm(total=len(self.dataset), smoothing=0)
+        for _ in range(len(self.dataset)):
+            idx, aug_text = out_queue.get(block=True)
+            pbar.update()
+            if isinstance(aug_text, Exception):
+                continue
+            if aug_text == "":
+                continue
+            augmented_indices.append(idx)
+            augmented_text.append(aug_text)
+
+        # Send sentinel values to worker processes
+        for _ in range(num_workers):
+            in_queue.put(("END", "END"))
+        worker_pool.terminate()
+        worker_pool.join()
+
+        augmented_indices = np.array(augmented_indices)
+        argsort_indices = np.argsort(augmented_indices)
+        augmented_text = [augmented_text[i] for i in argsort_indices]
+
+        return augmented_text
+
 
 def augment_from_queue(tx_ls, queue, augment):
-
-    from multiprocessing import current_process
-
-    name = current_process().name
-    #print(name, "Starting...")
     for text in tx_ls:
         result = (text[0], augment.augment(text[1])[0])
         queue.put(result)
-    #print(name, "Exiting...")
 
 
+def augment_from_queue_cpu(in_queue, out_queue, augmenter):
 
+    while True:
+        try:
+            i, inputs = in_queue.get()
+            if i == "END" and inputs == "END":
+                # End process when sentinel value is received
+                break
+            else:
+                if isinstance(inputs, tuple):
+                    text_to_augment = inputs[1]
+                else:
+                    text_to_augment = inputs
+
+                augmented_text = augmenter.augment(text_to_augment)
+
+                out_queue.put((i, augmented_text))
+        except Exception as e:
+            out_queue.put((i, e))
+
+
+def augment_from_queue_gpu(num_gpus, in_queue, out_queue, seed, augmenter):
+    gpu_id = (torch.multiprocessing.current_process()._identity[0] - 1) % num_gpus
+    set_seed(seed)
+    torch.cuda.set_device(gpu_id)
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
+    torch.cuda.set_device(gpu_id)
+
+    while True:
+        try:
+            i, inputs = in_queue.get()
+            if i == "END" and inputs == "END" :
+                # End process when sentinel value is received
+                break
+            else:
+                if isinstance(inputs, tuple):
+                    text_to_augment = inputs[1]
+                else:
+                    text_to_augment = inputs
+
+                augmented_text = augmenter.augment(text_to_augment)
+
+                out_queue.put((i, augmented_text))
+        except Exception as e:
+            out_queue.put((i, e))
+
+
+def set_seed(seed):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
