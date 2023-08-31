9eeef7950d07b7470e92c9e918a7097086eee062
HENG <hy345@exeter.ac.uk>
2023-08-31 01:00:06 +0100

    reformat

--- textattack/constraints/semantics/sentence_encoders/universal_sentence_encoder/universal_sentence_encoder.py
+++ textattack/constraints/semantics/sentence_encoders/universal_sentence_encoder/universal_sentence_encoder.py
@@ -35,7 +35,9 @@ class UniversalSentenceEncoder(SentenceEncoder):
             try:
                 self.model = hub.load(self._tfhub_url)
             except Exception as e:
-                print("Error loading model from tfhub, trying mirror url. Exception: ", e)
+                print(
+                    "Error loading model from tfhub, trying mirror url. Exception: ", e
+                )
                 self.model = hub.load(self.mirror_tfhub_url)
         return self.model(sentences).numpy()
 
