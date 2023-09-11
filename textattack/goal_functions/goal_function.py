9cdaa48b629f68295dc2ba086c2f73f5a869f32b
Yanjun Qi <qiyanjun2012@gmail.com>
2023-09-11 11:40:16 -0400

    make format update

--- textattack/goal_functions/goal_function.py
+++ textattack/goal_functions/goal_function.py
@@ -40,7 +40,7 @@ class GoalFunction(ReprMixin, ABC):
         use_cache=True,
         query_budget=float("inf"),
         model_batch_size=32,
-        model_cache_size=2 ** 20,
+        model_cache_size=2**20,
     ):
         validators.validate_model_goal_function_compatibility(
             self.__class__, model_wrapper.model.__class__
