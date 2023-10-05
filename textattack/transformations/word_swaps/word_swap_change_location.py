57864fc6c597fa17d1c620502e768cbeafba7b02
k-ivey <kivey1@gmail.com>
2023-10-05 18:00:03 -0400

    Add consistent parameter to name and location word swap

--- textattack/transformations/word_swaps/word_swap_change_location.py
+++ textattack/transformations/word_swaps/word_swap_change_location.py
@@ -2,6 +2,8 @@
 Word Swap by Changing Location
 -------------------------------
 """
+from collections import defaultdict
+
 import more_itertools as mit
 import numpy as np
 
@@ -25,12 +27,15 @@ def idx_to_words(ls, words):
 
 
 class WordSwapChangeLocation(WordSwap):
-    def __init__(self, n=3, confidence_score=0.7, language="en", **kwargs):
+    def __init__(
+        self, n=3, confidence_score=0.7, language="en", consistent=False, **kwargs
+    ):
         """Transformation that changes recognized locations of a sentence to
         another location that is given in the location map.
 
         :param n: Number of new locations to generate
         :param confidence_score: Location will only be changed if it's above the confidence score
+        :param consistent:  Whether to change all instances of the same location to the same new location
 
         >>> from textattack.transformations import WordSwapChangeLocation
         >>> from textattack.augmentation import Augmenter
@@ -44,6 +49,7 @@ class WordSwapChangeLocation(WordSwap):
         self.n = n
         self.confidence_score = confidence_score
         self.language = language
+        self.consistent = consistent
 
     def _get_transformations(self, current_text, indices_to_modify):
         words = current_text.words
@@ -64,26 +70,46 @@ class WordSwapChangeLocation(WordSwap):
         location_idx = [list(group) for group in mit.consecutive_groups(location_idx)]
         location_words = idx_to_words(location_idx, words)
 
+        if self.consistent:
+            location_to_indices = defaultdict(list)
+            for idx, location in location_words:
+                location_to_indices[self._capitalize(location)].append(idx[0])
+
         transformed_texts = []
         for location in location_words:
             idx = location[0]
-            word = location[1].capitalize()
+            word = self._capitalize(location[1])
             replacement_words = self._get_new_location(word)
             for r in replacement_words:
                 if r == word:
                     continue
-                text = current_text
 
-                # if original location is more than a single word, remain only the starting word
-                if len(idx) > 1:
-                    index = idx[1]
-                    for i in idx[1:]:
-                        text = text.delete_word_at_index(index)
+                if self.consistent:
+                    # If we're doing consistent replacements, only replace the word
+                    # if it hasn't already been replaced in a previous iteration
+                    if word not in location_to_indices:
+                        continue
 
-                # replace the starting word with new location
-                text = text.replace_word_at_index(idx[0], r)
+                    indices_to_delete = []
+                    if len(idx) > 1:
+                        for i in location_to_indices[word]:
+                            for j in range(1, len(idx)):
+                                indices_to_delete.append(i + j)
 
-                transformed_texts.append(text)
+                    transformed_texts.append(current_text.replace_words_at_indices(
+                        location_to_indices[word] + indices_to_delete,
+                        ([r] * len(location_to_indices[word]))
+                        + ([""] * len(indices_to_delete)),
+                    ))
+
+                    # Delete this word to mark it as replaced
+                    del location_to_indices[word]
+                else:
+                    # If the original location is more than a single word, keep only the starting word
+                    # and replace the starting word with the new word
+                    indices_to_delete = idx[1:]
+                    transformed_texts.append(current_text.replace_words_at_indices([idx[0]] + indices_to_delete, [r] + [""] * len(indices_to_delete)))
+
         return transformed_texts
 
     def _get_new_location(self, word):
@@ -101,3 +127,7 @@ class WordSwapChangeLocation(WordSwap):
         elif word in NAMED_ENTITIES["city"]:
             return np.random.choice(NAMED_ENTITIES["city"], self.n)
         return []
+
+    def _capitalize(self, string):
+        """Capitalizes all words in the string."""
+        return " ".join(word.capitalize() for word in string.split())
