67f4893d9a2dac1b956d3085f4cc6e15c5f9303c
Victor <victorlohwk@gmail.com>
2025-04-22 12:35:24 +0100

    Removed MaximizeBleu from init

--- textattack/goal_functions/text/__init__.py
+++ textattack/goal_functions/text/__init__.py
@@ -6,7 +6,6 @@ Goal Function for Text to Text case
 """
 
 from .minimize_bleu import MinimizeBleu
-from .maximize_bleu import MaximizeBleu
 from .non_overlapping_output import NonOverlappingOutput
 from .maximize_levenshtein import MaximizeLevenshtein
 from .text_to_text_goal_function import TextToTextGoalFunction
